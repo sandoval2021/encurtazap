ce1291aa875fec391b86388e197355928301f72d
sandoval2021 <78934835+sandoval2021@users.noreply.github.com>
2026-09-05 08:21:59 -0300

    chore: add Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  build: {
+    target: 'es2020',
+    sourcemap: false,
+    minify: true,
+  },
+
+  server: {
+    host: true,
+    port: 5173,
+  },
+
+  preview: {
+    host: true,
+    port: 4173,
+  },
+});
